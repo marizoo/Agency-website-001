2833faedc2e374cb1ffe3efcbf792381ce958eca
marizoo <marizoo@meow.woof>
2021-11-05 14:05:30 +0700

    initialize git, made navbar

---
+++ src/components/Intro.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: calc(100vh - 50px);
+`;
+
+const Container = styled.div`
+    height: calc(100vh - 50px);
+`;
+
+const Container = styled.div`
+    height: calc(100vh - 50px);
+`;
+
+const Intro = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Intro
