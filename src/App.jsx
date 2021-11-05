6444a26a7c72d6c487619a37dafd70fcc4d1bc05
marizoo <marizoo@meow.woof>
2021-11-05 17:22:07 +0700

    created main page &  animations

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,27 @@ import Navbar from "./components/Navbar";
 
 const Container = styled.div`
 height: 100vh;
+overflow: hidden;
 `;
+
+const IntroShape = styled.div`
+width: 100%;
+height: 100%;
+position: absolute;
+top: 0;
+left: 0;
+z-index: -1;
+clip-path: polygon(68% 0%, 100% 0%, 100% 100%, 47% 100%);
+background-color: crimson;
+
+`
 
 const App = () => {
   return (
      <Container>
         <Navbar />
         <Intro />
+        <IntroShape />
     </Container>
     );
 };
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,21 +1,97 @@
 import React from 'react'
 import styled from 'styled-components'
+import Woman from '../img/woman-sit-sm.png'
+import AnimatedShapes from './AnimatedShapes';
 
 const Container = styled.div`
     height: calc(100vh - 50px);
+    display: flex;
+    padding: 20px;
 `;
 
-const Container = styled.div`
-    height: calc(100vh - 50px);
+const Left = styled.div`
+width: 60%;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
 `;
 
-const Container = styled.div`
-    height: calc(100vh - 50px);
+const Title = styled.h1`
+width: 60%;
+font-size: 50px;
 `;
+
+const Desc = styled.p`
+width: 60%;
+font-size: 18px;
+margin-top: 20px;
+`;
+
+const Info = styled.div`
+width: 60%;
+margin-top: 50px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+`;
+
+const Button = styled.button`
+padding: 12px;
+background-color: darkblue;
+color: white;
+border-radius: 10px;
+font-weight: bold;
+border: none;
+letter-spacing: 2px;
+cursor: pointer;
+`;
+
+const Contact = styled.div`
+display: flex;
+flex-direction: column;
+font-size: .8rem;
+`;
+
+const Phone = styled.span`
+color: #f0667d;
+font-weight: bold;
+`;
+const ContactText = styled.span`
+color: gray;
+margin-top: 5px;
+`;
+
+const Right = styled.div`
+   width: 40%;
+`;
+
+const Image = styled.img`
+width: 80%;
+`
 
 const Intro = () => {
     return (
         <Container>
+            <Left>
+                <Title>Agency in creative age</Title>
+                <Desc>
+                    We believe that designing products and services in close partnership
+                    with our clients is the only way to have a real impact on their
+                    business.
+                </Desc>
+                <Info>
+                    <Button>START A PROJECT</Button>
+                    <Contact>
+                        <Phone>Call Us (012) 345 - 6789</Phone>
+                        <ContactText>For any question or concern</ContactText>
+                    </Contact>
+                </Info>
+            </Left>
+            <Right>
+                <Image src={Woman} />
+                <AnimatedShapes />
+            </Right>
             
         </Container>
     )
